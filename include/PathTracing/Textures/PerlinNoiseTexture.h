08c24e11a35b596999d1987daef1cd428b717001
agordeevw <agordeevw@gmail.com>
2018-12-13 00:10:15 +0700

    Implemented weird procedural texture

--- include/PathTracing/Textures/PerlinNoiseTexture.h
+++ include/PathTracing/Textures/PerlinNoiseTexture.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <glm/geometric.hpp>
 #include <glm/vec3.hpp>
 
 #include "PathTracing/Texture.h"
@@ -11,10 +12,23 @@ class PerlinNoiseTexture : public Texture {
   PerlinNoiseTexture(unsigned int seed, float scale)
       : noise(seed), scale(scale) {}
   glm::vec3 sample(float u, float v, const glm::vec3& p) const override {
-    return glm::vec3{0.5} * (1.0f + noise.sample(p * scale));
+    return glm::vec3{0.5} *
+           (1.0f + sinf(p.z * scale + 10.0f * turbulence(p * scale)));
   }
 
  private:
+  float turbulence(const glm::vec3& p, int depth = 7) const {
+    float accum = 0;
+    glm::vec3 tmp = p;
+    float weight = 1.0f;
+    for (int i = 0; i < depth; i++) {
+      accum += weight * noise.sample(tmp);
+      weight *= 0.5f;
+      tmp *= 2.0f;
+    }
+    return fabsf(accum);
+  }
+
   Util::PerlinNoise noise;
   float scale;
 };
