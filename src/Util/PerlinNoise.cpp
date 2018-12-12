f4c4ec4c355d021cf9a9925ddf31c4476703ff01
agordeevw <agordeevw@gmail.com>
2018-12-12 23:30:05 +0700

    Implemented smoothed noise

--- src/Util/PerlinNoise.cpp
+++ src/Util/PerlinNoise.cpp
@@ -24,10 +24,33 @@ PerlinNoise::PerlinNoise(unsigned int seed)
   std::shuffle(permutationZ.begin(), permutationZ.end(), randomEngine);
 }
 
+inline float trilinearInterpolation(float c[2][2][2], const glm::vec3& uvw) {
+  float accum = 0;
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 2; j++) {
+      for (int k = 0; k < 2; k++) {
+        glm::vec3 tmp = glm::mix(1.0f - uvw, uvw, glm::vec3{ i, j, k });
+        accum += tmp.x * tmp.y * tmp.z * c[i][j][k];
+      }
+    }
+  }
+  return accum;
+}
+
 float PerlinNoise::sample(const glm::vec3& p) const {
   glm::vec3 uvw = p - glm::floor(p);
-  glm::ivec3 ijk{4.0f * p};
-  ijk &= 255;
-  return values[permutationX[ijk[0]] ^ permutationY[ijk[1]] ^ permutationZ[ijk[2]]];
+  uvw = uvw * uvw * (3.0f - 2.0f * uvw);
+  glm::ivec3 ijk{glm::floor(p)};
+  float c[2][2][2];
+  for (int di = 0; di < 2; di++) {
+    for (int dj = 0; dj < 2; dj++) {
+      for (int dk = 0; dk < 2; dk++) {
+        c[di][dj][dk] = values[permutationX[(ijk[0] + di) & 255] ^
+                               permutationY[(ijk[1] + dj) & 255] ^
+                               permutationZ[(ijk[2] + dk) & 255]];
+      }
+    }
+  }
+  return trilinearInterpolation(c, uvw);
 }
 }  // namespace Util
